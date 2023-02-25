7d6b9869e18d83ef72148dda2daf877957d586a0
anup.kumar.tamang <anup.kumar.tamang@p2h.com>
2023-02-25 22:59:00 +0545

    feat(user-menu & LinkBtn): added user menu with login and created custom LinkButton

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,20 +1,30 @@
 import { Col, Row } from 'antd';
 import { Container, HeaderLayout } from 'components/UI';
 import { Logo, Nav } from 'components';
+import { UserMenu } from 'components/UserMenu/UserMenu';
 
 type Props = {};
 
 export const Header = (props: Props) => {
   return (
     <>
-      <HeaderLayout style={{ paddingInline: 0 }}>
+      <HeaderLayout style={{ paddingInline: 0, height: 'auto' }}>
         <Container>
           <Row>
             <Col xs={10} md={6}>
               <Logo />
             </Col>
             <Col xs={14} md={18}>
-              <Nav />
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                }}
+              >
+                <Nav />
+                <UserMenu />
+              </div>
             </Col>
           </Row>
         </Container>
