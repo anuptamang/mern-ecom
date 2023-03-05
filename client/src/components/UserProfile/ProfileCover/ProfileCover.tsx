c5bab0d709b579797f8e820a285119218aa52edb
anup.kumar.tamang <anup.kumar.tamang@p2h.com>
2023-03-05 21:31:00 +0545

    feat(User Dashboard): User dashbaord elements updated and profile section designs added

--- client/src/components/UserProfile/ProfileCover/ProfileCover.tsx
+++ client/src/components/UserProfile/ProfileCover/ProfileCover.tsx
@@ -1,3 +1,5 @@
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Upload, UploadProps } from 'antd';
 import { CoverPhoto } from 'components/UI/CoverPhoto';
 import React from 'react';
 
@@ -14,10 +16,29 @@ export const ProfileCover = ({
     onCoverPhotoChange('photoUrl');
   };
 
+  const props: UploadProps = {
+    action: '//jsonplaceholder.typicode.com/posts/',
+    listType: 'picture',
+    previewFile(file) {
+      console.log('Your upload file:', file);
+      // Your process logic. Here we just mock to the same file
+      return fetch('https://next.json-generator.com/api/json/get/4ytyBoLK8', {
+        method: 'POST',
+        body: file,
+      })
+        .then((res) => res.json())
+        .then(({ thumbnail }) => thumbnail);
+    },
+  };
+
   return (
     <div className="relative">
       <CoverPhoto coverPhotoUrl={coverPhotoUrl} />
-      <div className="absolute right-5 bottom-5 z-10">Change Cover Photo</div>
+      <div className="absolute right-5 bottom-5 z-[80]">
+        <Upload {...props}>
+          <Button icon={<UploadOutlined />}>Update Cover</Button>
+        </Upload>
+      </div>
     </div>
   );
 };
