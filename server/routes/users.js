ba5ab96ce5fb100e5b2889f567476db89227cd29
Anup K. Tamang <anupkumartamang@gmail.com>
2025-10-31 12:33:55 +0545

    fix (server-routes): move /stats route before /:id to prevent route conflict

--- server/routes/users.js
+++ server/routes/users.js
@@ -21,15 +21,14 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/register", registration);
 router.get("/list", getUsers);
-router.get("/:id", getUser);
-router.patch("/:id", Auth, updateUserProfile);
-router.delete("/:id", Auth, deleteUser);
-
-router.patch("/:id/profile-photo", Auth, Upload, uploadProfilePhoto);
-router.patch("/:id/cover-photo", Auth, Upload, uploadCoverPhoto);
-
 router.post("/check-user", validateUser);
 router.put("/change-password", createNewPassword);
 router.get("/stats", Auth, getUserStats);
+
+router.get("/:id", getUser);
+router.patch("/:id", Auth, updateUserProfile);
+router.delete("/:id", Auth, deleteUser);
+router.patch("/:id/profile-photo", Auth, Upload, uploadProfilePhoto);
+router.patch("/:id/cover-photo", Auth, Upload, uploadCoverPhoto);
 
 export default router;
