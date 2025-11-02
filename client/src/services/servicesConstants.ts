4e8ed1d0124bacbdd0c80ca780986e55ed4049af
Anup K. Tamang <anupkumartamang@gmail.com>
2025-11-02 19:41:51 +0545

    feat(frontend): add BANNERS_API constant
    - Add banner API endpoint to service constants

--- client/src/services/servicesConstants.ts
+++ client/src/services/servicesConstants.ts
@@ -3,3 +3,4 @@ import { BACKEND_API } from "configs/api";
 export const AUTH_API = `${BACKEND_API}/user`;
 export const PRODUCTS_API = `${BACKEND_API}/products`;
 export const CARTS_API = `${BACKEND_API}/carts`;
+export const BANNERS_API = `${BACKEND_API}/banners`;
